ec62b39a46b3ff53c4028fa4ecb79b314c0d03b3
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-11-05 18:35:55 -0700

    more appropriate api

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var $ = require('jquery')
-var dotenv = require("dotenv").config();
+
 var app = express();
 
 // view engine setup
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var dotenv = require("dotenv").config();
-const chuckKey = process.env.CHUCK_KEY 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Dave Wolfberg's Portfolio", chuckKey: chuckKey });
+  res.render('index', { title: "Dave Wolfberg's Portfolio"});
 });
 
 module.exports = router;
